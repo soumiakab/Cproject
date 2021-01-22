cb59f33f16c0b25526803becc2c88aa70c16794a
soumiakab <soumia111sk@gmail.com>
2021-01-22 13:57:13 +0100

    varGlobal

--- main.c
+++ main.c
@@ -1,28 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
+int t1[4], t2[4],s[4];
+int varDecimale;
 
 
 
-
-int sommeBinaire(int a[4],int b[4]){
-	int s[4];
+void sommeBinaire(){
 	
 	
-	
-	return s[4];
 }
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-int decToBin(int varBin){
+void decToBin(int varBin){
 	int varDecimale;
-return varDecimale;	
 }
 
-int binary number ( int Bnumber){
+void binarynumber ( int Bnumber){
 
-    int Bnumber, decimal_number, reminder;
+    int bnumber, decimal_number, reminder;
 
-  return 0;
 }
 
 int main(int argc, char *argv[]) {
