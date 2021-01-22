5b8d59621ff613a7fa76c0565bf4822698589105
saiess <saidessalami0@gmail.com>
2021-01-22 12:09:16 +0100

    ok

--- main.c
+++ main.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int binary number ( int Bnumber){
+int binary_number (int Bnumber){
 
     int Bnumber, decimal_number, reminder;
+    int binary_number[4];
 
   return 0;
 }
