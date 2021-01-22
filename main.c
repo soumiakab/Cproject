0774fae7c9f2370bf6e2f780b6474b7f67384efb
saiess <saidessalami0@gmail.com>
2021-01-22 10:39:18 +0100

    testing

--- main.c
+++ main.c
@@ -4,5 +4,22 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+    int binary_number, decimal_number, i=0, reminder;
+
+    printf("Enter a binary number");
+    scanf("%d", &binary_number);
+
+    for(i=0;i<=binary_number;i++)
+        print(i)
+
+
+
+
+
+
+
+
+
+
 	return 0;
 }
