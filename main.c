1624be14da5c00c58582358b42b9806296a5570c
saiess <saidessalami0@gmail.com>
2021-01-22 13:59:26 +0100

    okk

--- main.c
+++ main.c
@@ -6,25 +6,28 @@
 
 int sommeBinaire(int a[4],int b[4]){
 	int s[4];
-	
-	
-	
+
+
+
 	return s[4];
 }
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int decToBin(int varBin){
 	int varDecimale;
-return varDecimale;	
+return varDecimale;
 }
 
-int binary number ( int Bnumber){
-
-    int Bnumber, decimal_number, reminder;
-
+int BinToDec(int varBin){
+    int varDecimale=0;
+    for (int i=3;i>=0;i--){
+        varDecimale += s[i]*pow(2,(3-i));
+    }
+    printf("%d",varDecimale);
   return 0;
 }
 
 int main(int argc, char *argv[]) {
+
 	return 0;
 }
