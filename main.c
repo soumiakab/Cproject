96da8c21bdc63866f189aa46885a9180c2ca36bb
soumiakab <soumia111sk@gmail.com>
2021-01-22 11:32:11 +0100

    ll

--- main.c
+++ main.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+
+
+int sommeBinaire(int a[4],int b[4]){
+	int s[4];
+	
+	
+	
+	return s[4];
+}
+
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
+	
+	
 	return 0;
 }
