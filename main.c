796bd59b54b0d5ae3d69e3170cf9f75788e5a3f7
ilyass <tahzima.i@gmail.com>
2021-01-22 10:15:20 +0100

    la fonction de convertion de decimale to binaire

--- main.c
+++ main.c
@@ -2,6 +2,10 @@
 #include <stdlib.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
+int decToBin(int varBin){
+
+return 0;	
+}
 
 int main(int argc, char *argv[]) {
 	return 0;
