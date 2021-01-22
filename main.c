b233f5bac469cfb841eefe2382fa15f2060f8f9f
saiess <saidessalami0@gmail.com>
2021-01-22 11:09:46 +0100

    define function

--- main.c
+++ main.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
+int binary number ( int Bnumber){
+
+    int Bnumber, decimal_number, reminder;
+
+  return 0;
+}
+
 int main(int argc, char *argv[]) {
-    int binary_number, decimal_number, i=0, reminder;
-
-    printf("Enter a binary number");
-    scanf("%d", &binary_number);
-
-    for(i=0;i<=binary_number;i++)
-        print(i)
-
-
-
-
-
-
 
 
 
