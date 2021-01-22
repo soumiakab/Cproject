5f953449167adfe52b4f141ad3880f54399301d4
ilyass <tahzima.i@gmail.com>
2021-01-22 10:30:32 +0100

    test de push

--- main.c
+++ main.c
@@ -3,8 +3,8 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 int decToBin(int varBin){
-
-return 0;	
+	int varDecimale;
+return varDecimale;	
 }
 
 int main(int argc, char *argv[]) {
