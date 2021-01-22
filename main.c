661ef6643904365f6b5f8d585457301cd5484f53
soumiakab <soumia111sk@gmail.com>
2021-01-22 15:08:06 +0100

    sommeinst

--- main.c
+++ main.c
@@ -1,28 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-int t1[4], t2[4],s[4];
+int ind=4;
+int t1[4], t2[4],s[5];
+
 int varDecimale;
 
 
 
 void sommeBinaire(){
-	for(int i=3;i>=0;i--){
+
+	for(int i=0;i<=ind;i++){
+		s[i]=0;
+	}
+	
+	for(int i=ind-1;i>0;i--){
 		if((t1[i]+t2[i])==2){
 			t1[i-1]+=1;
-			s[i]=0;
+			s[i+1]=0;
 		}
 		else{
 			if((t1[i]+t2[i])>2){
 			t1[i-1]+=1;
-			s[i]=1;
+			s[i+1]=1;
 			}
 		else{
-			
-				s[i]=t1[i]+t2[i];
-				
+		
+			s[i+1]=t1[i]+t2[i];
 			}
 		}
+		int var;
+		if((t1[0]+t2[0])>1){
+			s[0]=1;
+			
+		}
+		
 	}
+	if(s[0]==1){
+		printf("%d",s[0]);
+	}
+	for(int i=1;i<=ind;i++){
+		printf("%d",s[i]);
+	}
+	
 	
 }
 
